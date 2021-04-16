e7b3d93e61b20a15a584b6ab212851f0ec315ad4
Kiyoshi Kudo <kiyoshi.kudo20@gmail.com>
2021-04-16 19:59:57 +0900

    Implement MyPage

---
+++ src/main/java/com/example/springsecuritytutorial/controller/mypage/MyPageController.java
@@ -0,0 +1,15 @@
+package com.example.springsecuritytutorial.controller.mypage;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/mypage/")
+public class MyPageController {
+
+    @GetMapping
+    public String show() {
+        return "mypage/show";
+    }
+}
